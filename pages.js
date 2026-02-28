e2fd3a355c1c3d0e6d0e2cb1f17c31a8342289c1
x267Git <s.kharianov@yandex.ru>
2026-02-28 22:12:47 +0300

    version-1.01
    chore

--- pages.js
+++ pages.js
@@ -1,24 +1,28 @@
 const pages = document.querySelectorAll(".page");
 
-let buttons, arrow
-
+let buttons, arrow, overlay
+let active = false
 if (window.matchMedia("(max-width: 768px)").matches) {
+
+    overlay = document.querySelector(".overlay")
     buttons = document.querySelectorAll(".side-button");
     arrow = document.querySelector(".side-menu-arrow");
-    setTimeout(() => { arrow.style.top = "130px" }, 400)
+
+    setTimeout(() => { arrow.style.top = "50px" }, 400)
+
     const object = document.querySelector('.my-object');
-    setInterval(upd_arrow, 100)
     const menu = document.querySelector(".side-menu")
-    arrow.addEventListener("click", function () {
-        if (this.classList.contains("active")) {
-            this.classList.remove("active");
-            menu.classList.remove("active");
-        }
-        else {
-            this.classList.add("active");
-            menu.classList.add("active");
-        }
-    });
+
+    arrow.addEventListener("click", open_close);
+    overlay.addEventListener("click", open_close)
+
+    const el_list = [arrow, menu, overlay]
+
+    function open_close() {
+        if (active) { el_list.forEach(el => { el.classList.remove("active") }) }
+        else { el_list.forEach(el => { el.classList.add("active") }) }
+        active = !active
+    }
 }
 else { buttons = document.querySelectorAll(".button, .basic"); }
 
@@ -33,9 +37,3 @@ buttons.forEach(button => {
         page.classList.add("active");
     })
 });
-
-function upd_arrow() {
-    const stayScroll = (document.documentElement.scrollHeight - window.innerHeight) - window.scrollY
-    if (stayScroll < 300) { arrow.style.width = (40 + 100 - stayScroll / 3) + 'px'; }
-    else { arrow.style.width = "40px" }
-};
