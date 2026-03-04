f5cd86ef8e5febfd15bd4457a095889c7414c821
x267Git <s.kharianov@yandex.ru>
2026-03-04 18:35:53 +0300

    version-1.05
    chore

--- pages.js
+++ pages.js
@@ -34,7 +34,7 @@ buttons.forEach(button => {
     button.addEventListener("click", function () {
         buttons.forEach(button => { button.classList.remove("pressed"); })
         button.classList.add("pressed");
-        pages.forEach(page => { page.classList.remove("active"); page.scrollTop = 0; })
+        pages.forEach(page => { page.classList.remove("active"); })
         const page_id = button.getAttribute("page-id");
         const page = document.querySelector(`.page[page-id="${page_id}"`)
         page.classList.add("active");
